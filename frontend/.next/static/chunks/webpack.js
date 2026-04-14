908f06d8490a4975d93528ace546f4477b70f966
eralisteep <eralirahat@gmail.com>
2026-04-14 10:37:02 +0500

    pdf download

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -163,7 +163,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c761e834a8dc1d24"; }
+/******/ 		__webpack_require__.h = function() { return "802954df66c9fe41"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
